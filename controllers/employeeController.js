69765fa74de9c35b57827ec48fdf7b840800436e
Jason <limaeight@gmail.com>
2021-01-30 02:22:55 -0500

    add post method

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -10,4 +10,28 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with the given id: ${req.params.id}`)
+        
+    Employee.findById(req.params.id, (err, docs) => {
+        if (!err) { res.send(docs) }
+        else { console.log("Error in retrieving Employees:" + JSON.stringify(err, undefined, 2)) }
+    })
+})
+
+router.post('/', (req, res) => {
+    var emp = new Employee ({
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary,
+    })
+    emp.save((err, doc) => {
+        if (!err) { res.send(docs) }
+        else { console.log("Error in retrieving Employees:" + JSON.stringify(err, undefined, 2)) }
+    })
+})
+
+
 module.exports = router
