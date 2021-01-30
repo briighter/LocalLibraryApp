21a57dd4998d124797a10fe435527b401c8c1ce7
Jason <jaylima0@gmail.com>
2021-01-30 18:24:11 -0500

    clean

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -10,4 +10,17 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    var emp = new Employee({
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary,
+    })
+    emp.save((err, doc) => {
+        if (!err) { res.send(docs) }
+        else { console.log("Error in retrieving Employees:" + JSON.stringify(err, undefined, 2)) }
+    })
+})
+
 module.exports = router
