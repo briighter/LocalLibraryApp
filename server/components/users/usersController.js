31faf10c3800bee343579000a164fa75e4b1a18c
Jason <jaylima0@gmail.com>
2021-06-16 17:58:49 -0400

    model

--- server/components/users/usersController.js
+++ server/components/users/usersController.js
@@ -1,6 +1,7 @@
 const User = require('./user')
 const { userService } = require('./usersService')
 
+var User = new User
 // Display list of all Users.
 exports.user_list = function (req, res) {
     res.send('NOT IMPLEMENTED: User list');
