1c775b7f0a6c6e077eeb4c7f3193a9f2f64b577e
Jason <jaylima0@gmail.com>
2021-06-27 08:42:43 -0400

    clean

--- server/components/authors/author.js
+++ server/components/authors/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
 
 var Schema = mongoose.Schema;
 
@@ -20,7 +21,7 @@ authorSchema
 authorSchema
     .virtual('lifespan')
     .get(function () {
-        return (this.date_of_death.getFullYear() - this.date_of_birth.getFullYear()).toString();
+        return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
     });
 
 // Virtual for author's URL
