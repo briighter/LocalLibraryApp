4d4b9e36f650ede1cbfa187075ae6d3c1231117c
Jason <limaeight@gmail.com>
2021-07-09 22:43:49 -0400

    clean

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,5 @@
 //Import the mongoose module
 var mongoose = require('mongoose');
-mongoose.set('bufferCommands', false);
 
 //Set up default mongoose connection
 var uri = 'mongodb+srv://admin:RAzIXIrKaJsGmkCA@cluster0.ucpvo.mongodb.net/localLibrary?retryWrites=true&w=majority';
